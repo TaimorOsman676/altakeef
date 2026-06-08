62f3cd01311e879a436bcb691c8c74c351517839
Taimor Osman <77933110+Taimorosman@users.noreply.github.com>
2026-06-09 00:45:26 +0300

    feat(seo/gtm): update GTM ID to GTM-TNXC3PNF

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -74,7 +74,7 @@ export default async function LocaleLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KMWFBZ7R');`
+})(window,document,'script','dataLayer','GTM-TNXC3PNF');`
           }}
         />
         
@@ -167,7 +167,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KMWFBZ7R"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TNXC3PNF"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
