52d7484fffda9958db59773bd393c155d0322d75
Taimor Osman <77933110+Taimorosman@users.noreply.github.com>
2026-06-14 01:47:02 +0300

    feat(seo/gtm): install Google Tag AW-18010504865

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -176,7 +176,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google tag (gtag.js) */}
         <Script
           strategy="lazyOnload"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18012327894"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18010504865"
         />
         <Script
           id="google-analytics"
@@ -186,6 +186,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
+              gtag('config', 'AW-18010504865');
               gtag('config', 'AW-18012327894');
             `
           }}
