40c6ff240c1900a590ac48cf133fe122887c8955
Toobasid <contenthaul@gmail.com>
2026-06-24 17:48:40 +0500

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -79,6 +79,13 @@ export default function Header() {
 
   return (
     <header className="fixed inset-x-0 top-0 z-50" dir={isRTL ? 'rtl' : 'ltr'}>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-P83P7HJC');</script>
+      <!-- End Google Tag Manager -->
       {/* ================================================================= */}
       {/* TOP UTILITY BAR (SUB HEADER)                                      */}
       {/* ================================================================= */}
